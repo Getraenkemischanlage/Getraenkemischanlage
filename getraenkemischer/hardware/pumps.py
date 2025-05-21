80e131e124e5bbb7a4217ec9a5faad6cbff71858
Laurin Oberlader <oberlader.laurin@gmail.com>
2025-05-21 12:25:22 +0200

    jpo

---
+++ getraenkemischer/hardware/pumps.py
@@ -0,0 +1,5 @@
+# Pumpe 1: GP 22
+# Pumpe 2: GP 21
+# Pumpe 3: GP 20
+# Pumpe 4: GP 19
+# Pumpe 5: GP 18
---
+++ getraenkemischer/hardware/sensor_interface.py
@@ -0,0 +1,5 @@
+# Sensor 1: GP 0
+# Sensor 2: GP 1
+# Sensor 3: GP 2
+# Sensor 4: GP 3
+# Sensor 5: GP 4
