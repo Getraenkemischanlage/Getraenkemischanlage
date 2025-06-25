cdd6440ff5df7ee808b489a173f0cef234e6984f
Dean <dean18azar@gmail.com>
2025-06-25 16:19:09 +0200

    sensor update

--- getraenkemischer/sensor_manager.py
+++ getraenkemischer/sensor_manager.py
@@ -1,25 +1,8 @@
-'''
-Funktion:
-- Liest die Sensoren ein 
-- Berechnet den Füllstand der Behälter
-- Gibt die Füllstände der Behälter zurück
-
-Klassen:
-- SensorManager
-    Klassenobjekte:
-        - sensor_pins: dict
-        - fill_level: dict
-    Klassenmethoden:
-        - __init__()
-        - read_sensors() (gibt dict zurücj mit Zuordnung Pumpe:Pin)
-        - calculate_fill_level
-'''
-
 from config import sensor_pins
 from machine import Pin
 import time
 
-# Vereinfachter HX711-Treiber für den Test
+# Vereinfachter HX711-Treiber für Pico
 class HX711:
     def __init__(self, dout, pd_sck):
         self.dout = Pin(dout, Pin.IN, pull=Pin.PULL_UP)
@@ -46,83 +29,43 @@ class HX711:
 
         return count
 
-'''
-# Test-Pins
-dout_pin = 1  # DOUT an GP2
-sck_pin = 0   # SCK an GP3
-
-# HX711 initialisieren
-hx = HX711(dout=dout_pin, pd_sck=sck_pin)
-'''
 
 class SensorManager:
     def __init__(self):
         self.sensor_pins = sensor_pins
-        self.gewicht_in_gramm = []
-        self.zuordnung = {}  # Zuordnung von Behälter zu Gewichten
-       
+
     def read_fill_levels(self):
-        for i in range(1, 4):
-            dout = list(self.sensor_pins.values())[i]
+        self.gewicht_in_gramm = []
+        self.zuordnung = {}
+
+        # Sensoren außer "SCK" (angenommen: SCK ist für alle gleich)
+        sensor_keys = list(self.sensor_pins.keys())
+        data_keys = sensor_keys[1:]  # ["Wasser", "Sirup_a", "Sirup_b", "Alkohol"]
+
+        for key in data_keys:
+            dout = self.sensor_pins[key]
             pd_sck = self.sensor_pins["SCK"]
-            print(f"dout: {dout}, pd_sck: {pd_sck}")
-            hx = HX711(dout, pd_sck)  
+            print(f"[{key}] DOUT: {dout}, SCK: {pd_sck}")
 
-            print(f"Starte Messung Sensor {list(self.sensor_pins.keys())[i]} ")
+            hx = HX711(dout, pd_sck)
+            rohwerte = []
 
-            rohwerte = []  # Liste für Rohwerte
-            for _ in range(10):                 # Anzahl der Messungen pro Sensor
-                rohwert = hx.read()             # Rohwert vom HX711 lesen
-                rohwerte.append(rohwert)  
+            for _ in range(10):
+                rohwert = hx.read()
+                rohwerte.append(rohwert)
                 time.sleep(0.5)
-            
-            durchschittswert = sum(rohwerte) // len(rohwerte)  # Durchschnitt der Rohwerte
 
-            gain = 1300 / (6584035.0 - 7903406.0)  # Beispielwert für Gain
+            durchschnitt = sum(rohwerte) // len(rohwerte)
+
+            # Beispielhafte Kalibrierung – muss angepasst werden
+            gain = 1300 / (6584035.0 - 7903406.0)
             offset = 7903406.0
+            gewicht = gain * (durchschnitt - offset)
 
-            self.gewicht_in_gramm.append(gain * (durchschittswert - offset))
+            self.gewicht_in_gramm.append(gewicht)
+            self.zuordnung[key] = gewicht
 
-            for i in range(len(self.gewicht_in_gramm)):
-                self.zuordnung["Sensor_" + str(i)] = self.gewicht_in_gramm[i]
-                
-            print(f"Rohwert: {durchschittswert}, Gewicht in Gramm (ungefähr!): {self.gewicht_in_gramm:.2f}")
+            print(f"[{key}] Rohwert: {durchschnitt}, Gewicht (ca.): {gewicht:.2f} g")
             time.sleep(0.5)
 
         return self.zuordnung
-
-'''
-    def calculate_fill_level(self):
-        for name, weight in self.read_sensors().items():
-            # Beispiel: 1g = 1ml, anpassen je nach Behälter!
-            fill_level[name] = max(0, int(weight))
-        return fill_level
-'''
-
-'''
-# Endlosschleife zum Testen
-
-print("Starte Messung...")
-
-while True:
-    rohwert = hx.read()
-
-    gain = 1300 / (1.160204e+07 - 1.156051e+07)  
-    offset = 1.156051e+07
-
-    gewicht_in_gramm = gain * (rohwert - offset)
-
-    print(f"Rohwert: {rohwert}, Gewicht in Gramm (ungefähr!): {gewicht_in_gramm:.2f}")
-    time.sleep(0.5)
-
-
-# Methode zum Kalibrieren
-
-for i in range(20):
-    liste = []
-    liste.append(hx.read())
-    time.sleep(0.5)
-    durchschnitt = sum(liste) / len(liste)
-
-print(durchschnitt)
-'''
